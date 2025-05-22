16c6567b2c53faa7f7de23a8cb4ec9d85adb43b6
Fernando Rossi <fenobrerossi@gmail.com>
2025-05-22 13:58:07 -0300

    corrected setup

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.12.2', # Specify minimum Python version
-    entry_points={'console_scripts': ['infernommatic = infernommatic.cli:main']}
+    entry_points={'console_scripts': ['infernommatic = infernommatic.infernommatic:main']}
 )
 
